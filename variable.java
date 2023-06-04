0013f0735eff44c66527ca3b26c9302122e7347c
golfhiro <tamabolt24@gmail.com>
2023-06-04 21:29:35 +0900

    variable

---
+++ variable.java
@@ -0,0 +1,15 @@
+class variable {
+  public static void main (String[] args){
+    int var = 1; //整数の場合は、int
+    int var_1 = 2;
+    String var02 = "hello world"; //文字列　string
+    boolean var_bool;
+
+    var_bool = (var < var_1);
+
+    System.out.println(var);
+    System.out.println(var_1);
+    System.out.println(var02);
+    System.out.println(var_bool);
+  }
+}
