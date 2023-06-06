9cfa7f211affce5caa1e01e6f86cb28bd68103b1
golfhiro <tamabolt24@gmail.com>
2023-06-06 16:05:24 +0900

    演算子

---
+++ operator.java
@@ -0,0 +1,17 @@
+public class operator {
+  public static void main(String[] args) {
+    //演算子
+    int x = 10;
+    int y = 2;
+
+    System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+    System.out.println(x/y);
+    System.out.println(x%y);
+
+    //関係演算子
+    System.out.println(x > y);
+    System.out.println(x < y);
+  }
+}
