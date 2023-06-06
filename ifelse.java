5127bd8f0ce0e112930ca088787621423d8b2c6b
golfhiro <tamabolt24@gmail.com>
2023-06-06 16:12:40 +0900

    if文

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+  public static void main(String[] args) {
+
+    int age = 18;
+
+    if (age >= 20) {
+      System.out.println("adult");
+    } else {
+      System.out.println("child");
+    }
+  }
+}
